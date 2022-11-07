610ef541f1178d74d67ff318f00833c45d96949c
koko9nut6 <alexko96@naver.com>
2022-11-07 19:00:36 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/Member.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity;
+
+public class User {
+    private Integer id;
+    private String password;
+    private String email;
+
+    private List<Role> roles;
+}
